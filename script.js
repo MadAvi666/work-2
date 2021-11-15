f850c3af2e25c6013ca1d8148d1fc08094ea8b92
Detdomovec228 <92791097+Detdomovec228@users.noreply.github.com>
2021-11-16 03:08:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const hamburger = document.querySelector(".header-hamburger");
 const menu = document.querySelector(".header-drop-list");
+const loadbutton = document.querySelector(".load-more-button-frame");
+const loadbuttoncontainer = document.querySelector(".load-more-button-container");
+const hiddenCards = document.querySelectorAll('.completed-airdrops-frame.ca-frame-hidden')
 
 hamburger.addEventListener("click", function() {
   menu.classList.toggle("opened");
+});
+
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("hamburger-rotate");
+});
+
+loadbutton.addEventListener("click", function() {
+  hiddenCards.forEach(function(card) {
+    card.classList.remove('ca-frame-hidden')
+  })
+  loadbuttoncontainer.classList.add("ca-frame-hidden")  
 });
